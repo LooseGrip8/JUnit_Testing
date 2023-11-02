b19f1a69d73ee2b963b1646f3f0c6cce6e602a38
LooseGrip8 <dzikrathoriq673@gmail.com>
2023-11-02 20:08:06 +0700

    fixed some incorrect comments

--- src/main/java/org/example/calculator.java
+++ src/main/java/org/example/calculator.java
@@ -2,13 +2,16 @@ package org.example;
 import java.util.stream.DoubleStream;
 
 public class calculator {
-
+    //    Method untuk penjumlahan
     static double add(double... operands) {
+        // Menggunakan DoubleStream untuk menjumlahkan semua operan
         return DoubleStream.of(operands)
                 .sum();
     }
-
+    //    Meethod untuk perkalian
     static double multiply(double... operands) {
+        // Menggunakan DoubleStream dan reduce() untuk mengalikan semua operan
+        // Nilai awal adalah 1, sehingga operasi perkalian dimulai dari 1
         return DoubleStream.of(operands)
                 .reduce(1, (a, b) -> a * b);
     }
