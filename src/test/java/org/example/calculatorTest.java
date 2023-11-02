c0b55a35602b37f41355ebace50cf2ea17794717
LooseGrip8 <dzikrathoriq673@gmail.com>
2023-11-02 19:56:26 +0700

    added some comments to explain the code

--- src/test/java/org/example/calculatorTest.java
+++ src/test/java/org/example/calculatorTest.java
@@ -7,12 +7,17 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class calculatorTest {
 
+    //Method yang digunakan untuk penjumlahan
     @Test
     @DisplayName("Add two numbers")
+
+    //void add() merupakan method untuk melakukan penjumlahan
+    //asssertEquals merupakan asersi (pemasukan) yang memeriksa apakah hasil sama dengan ekspektasi (2+2 = 4)
     void add() {
         assertEquals(4, calculator.add(2, 2));
     }
 
+    //Method yang digunakan untuk perkalian
     @Test
     @DisplayName("Multiply two numbers")
     void multiply() {
@@ -20,5 +25,9 @@ class calculatorTest {
                 () -> assertEquals(-4, calculator.multiply(2, -2)),
                 () -> assertEquals(4, calculator.multiply(-2, -2)),
                 () -> assertEquals(0, calculator.multiply(1, 0)));
+
+        //assertAll(): Ini adalah metode yang digunakan untuk mengelompokkan beberapa asersi (assertions) bersama-sama.
+        //Dalam kode tersebut, kita ingin melakukan beberapa asersi dalam satu metode pengujian, jadi assertAll digunakan untuk mengelompokkannya.
+        //Ada tiga asersi tambahan yang melakukan hal yang sama untuk berbagai kombinasi angka positif dan negatif serta perkalian dengan 0.
     }
 }
